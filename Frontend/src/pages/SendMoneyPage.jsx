92e2185860288433e15fb9334ca5b65f905dfb5a
Amol <amolgawade56@gmail.com>
2025-01-27 23:45:54 +0530

    Pages Add for application routes

---
+++ Frontend/src/pages/SendMoneyPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Send } from '../components/index.js'
+
+function SendMoneyPage() {
+  return (
+    <div>
+        <Send/>
+    </div>
+  )
+}
+
+export default SendMoneyPage
